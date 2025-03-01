49dce0c54d7a8fbd78025479f63e7e73cca08dc9
Jaakko Hirvioja <jhirvioja@gmail.com>
2025-03-01 18:50:51 +0200

    Add previews

--- app/src/main/java/fi/voicehaus/diceroller/ui/ShowResultsScreen.kt
+++ app/src/main/java/fi/voicehaus/diceroller/ui/ShowResultsScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import fi.voicehaus.diceroller.R
 
+@Preview
 @Composable
 fun ShowResultsScreen(
     modifier: Modifier = Modifier
--- app/src/main/java/fi/voicehaus/diceroller/ui/StartDiceRollScreen.kt
+++ app/src/main/java/fi/voicehaus/diceroller/ui/StartDiceRollScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fi.voicehaus.diceroller.R
 import kotlinx.coroutines.delay
@@ -95,4 +96,12 @@ fun StartDiceRollScreen(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun StartDiceRollScreenPreview() {
+    StartDiceRollScreen(
+        onShowResultsClicked = {}
+    )
 }
