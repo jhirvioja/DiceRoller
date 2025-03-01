d7c10f68ad0db3f2ec844bee5f54923735ef0ecd
Jaakko Hirvioja <jhirvioja@gmail.com>
2025-03-01 11:48:41 +0200

    Add images of dice, add to MainActivity

--- app/src/main/java/fi/voicehaus/diceroller/MainActivity.kt
+++ app/src/main/java/fi/voicehaus/diceroller/MainActivity.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fi.voicehaus.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,13 +45,17 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    Column (
+    Column(
         modifier = Modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
         }
     }
 }
-
