2b9799b7198fb4048d2b7834cb668e45bb4e9c5b
Jaakko Hirvioja <jhirvioja@gmail.com>
2025-03-01 17:21:14 +0200

    Add loading indicator, simulate rolling w/ delay

--- app/src/main/java/fi/voicehaus/diceroller/MainActivity.kt
+++ app/src/main/java/fi/voicehaus/diceroller/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -22,9 +23,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,6 +37,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fi.voicehaus.diceroller.ui.theme.DiceRollerTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,8 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceRollerTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     DiceRollerApp()
                 }
@@ -53,11 +58,9 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
-    Scaffold(
-        topBar = {
-            DiceRollerTopAppBar()
-        }
-    ) { paddingValues ->
+    Scaffold(topBar = {
+        DiceRollerTopAppBar()
+    }) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -73,27 +76,27 @@ fun DiceRollerApp() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DiceRollerTopAppBar() {
-    CenterAlignedTopAppBar(
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.primary,
-        ),
-        title = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.headlineSmall
-                )
-            }
+    CenterAlignedTopAppBar(colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        titleContentColor = MaterialTheme.colorScheme.primary,
+    ), title = {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
+            )
         }
-    )
+    })
 }
 
 @Composable
 fun DiceWithButtonAndImage() {
     var result by remember { mutableIntStateOf(1) }
+    var isRolling by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+
     val imageResource = when (result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
@@ -102,22 +105,40 @@ fun DiceWithButtonAndImage() {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
+    
     Column(
         modifier = Modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Image(
-            painter = painterResource(imageResource),
-            contentDescription = result.toString()
+            painter = painterResource(imageResource), contentDescription = result.toString()
         )
         Spacer(modifier = Modifier.height(16.dp))
         Row(
             horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Button(onClick = { result = (1..6).random() }) {
-                Text(stringResource(R.string.roll))
+            Button(
+                modifier = Modifier.height(42.dp), onClick = {
+                    if (!isRolling) {
+                        isRolling = true
+                        coroutineScope.launch {
+                            delay(2000)
+                            result = (1..6).random()
+                            isRolling = false
+                        }
+                    }
+                }, enabled = !isRolling
+            ) {
+                if (isRolling) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.width(24.dp),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                } else {
+                    Text(stringResource(R.string.roll))
+                }
             }
             Spacer(modifier = Modifier.width(8.dp))
             OutlinedButton(onClick = { /* TODO */ }) {
